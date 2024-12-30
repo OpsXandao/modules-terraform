7677223538470898cc6499bff841051e1d2ab102
Alexandre Paiva <alexandremarcio1504@gmail.com>
2024-12-30 10:06:16 -0300

    module ecs

---
+++ terraform/ecs/variables.tf
@@ -0,0 +1,20 @@
+variable "cluster_name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+variable "container_image" {
+  type = string
+}
+
+variable "cidr_block" {
+  description = "Bloco CIDR da VPC"
+  type        = string
+}
